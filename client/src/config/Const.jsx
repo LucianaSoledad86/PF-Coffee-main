d01bec229c72b3b46daf478d6449b6eef350618c
Luciana <97133350+LucianaSoledad86@users.noreply.github.com>
2022-10-06 00:05:42 -0300

    Update Const.jsx

--- client/src/config/Const.jsx
+++ client/src/config/Const.jsx
@@ -1,6 +1,6 @@
 //--------Comentar por el puerto a no usar------------------------
 //DB
-export const URL = 'http://localhost:3001';
+export const URL = 'https://pfcoffee-app.herokuapp.com/';
 //HEROKU
 // export const URL = 'https://pfcoffee-app.herokuapp.com';
 //console.log(URL === 'http://localhost:3001' ? "DB" : "HEROKU");
